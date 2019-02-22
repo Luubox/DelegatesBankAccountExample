367c889a06701c5aa97e2ccc8c947b8e75a8a1b4
J Nielsen <buhros@hotmail.com>
2019-02-22 09:53:50 +0100

    Done

---
+++ DelegatesBankAccountExample/Account.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegatesBankAccountExample
+{
+    class Account
+    {
+        private int _balance;
+
+        public delegate int Deposit(int deposit);
+        public delegate int Withdrawal(int withdrawal);
+
+        public Deposit AccountDeposit;
+        public Withdrawal AccountWithdrawal;
+
+        public int DepositRegular(int deposit)
+        {
+            _balance += deposit;
+            return Balance;
+        }
+        public int DepositWhiteWash(int deposit)
+        {
+            if (deposit > 1000)
+            {
+                Console.WriteLine("Error!");
+                return Balance;
+            }
+            _balance += deposit;
+            return Balance;
+        }
+
+        public int WithdrawalRegular(int withdrawal)
+        {
+            _balance -= withdrawal;
+            return Balance;
+        }
+
+        public int WithdrawalMaxThousand(int withdrawal)
+        {
+            if (withdrawal > 1000)
+            {
+                Console.WriteLine("Error!");
+                return Balance;
+            }
+            _balance -= withdrawal;
+            return Balance;
+        }
+
+        public int WithdrawalDebit(int withdrawal)
+        {
+            if (withdrawal > _balance)
+            {
+                Console.WriteLine("Error!");
+                return Balance;
+            }
+            _balance -= withdrawal;
+            return Balance;
+        }
+
+        public int Balance
+        {
+            get { return _balance; }
+        }
+
+    }
+}
--- DelegatesBankAccountExample/Program.cs
+++ DelegatesBankAccountExample/Program.cs
@@ -10,6 +10,21 @@ namespace DelegatesBankAccountExample
     {
         static void Main(string[] args)
         {
+            Account nikolajsAccount = new Account();
+            Account johansAccount = new Account();
+
+            nikolajsAccount.AccountDeposit = nikolajsAccount.DepositRegular;
+            nikolajsAccount.AccountWithdrawal =
+                nikolajsAccount.WithdrawalRegular;
+
+            johansAccount.AccountDeposit = johansAccount.DepositWhiteWash;
+            johansAccount.AccountWithdrawal = johansAccount.WithdrawalDebit;
+
+            Console.WriteLine(nikolajsAccount.AccountDeposit(3));
+            Console.WriteLine(johansAccount.AccountDeposit(1001));
+
+            Console.WriteLine(nikolajsAccount.AccountDeposit(10));
+            Console.WriteLine(johansAccount.AccountWithdrawal(1000));
         }
     }
 }
